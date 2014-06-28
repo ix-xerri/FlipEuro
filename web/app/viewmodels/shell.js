44a0b0e2641c198cc2777596a53dcb9649338bb8
ix-xerri <matthewxerri@Matthews-MacBook-Pro.local>
2014-06-28 19:07:03 +0200

    Work on login, register and account pages
    Working on the existing pages to improve functionality

--- web/app/viewmodels/shell.js
+++ web/app/viewmodels/shell.js
@@ -5,7 +5,9 @@ define(['plugins/router', "durandal/app"], function (router, app) {
         activate: function () {
             router.map([
                 {"route":"","moduleId":"viewmodels/login","title":"Login","nav":true},
+                {"route":"login","moduleId":"viewmodels/login","title":"Login","nav":true},
                 {"route":"register","moduleId":"viewmodels/register","title":"Register","nav":true},
+                {"route":"account","moduleId":"viewmodels/account","title":"Account","nav":false},
                 /*{durandal:routes}*/
             ]).buildNavigationModel();
             
