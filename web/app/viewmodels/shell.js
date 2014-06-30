fcd335b9aa6da82491490102a1621a0d0f6fb509
ix-xerri <matthewxerri@matthews-mbp.lan>
2014-06-30 19:18:28 +0200

    New routes

--- web/app/viewmodels/shell.js
+++ web/app/viewmodels/shell.js
@@ -8,6 +8,8 @@ define(['plugins/router', "durandal/app"], function (router, app) {
                 {"route":"login","moduleId":"viewmodels/login","title":"Login","nav":true},
                 {"route":"register","moduleId":"viewmodels/register","title":"Register","nav":true},
                 {"route":"account","moduleId":"viewmodels/account","title":"Account","nav":false},
+                {"route":"admin","moduleId":"viewmodels/admin","title":"Admin","nav":true},
+                {"route":"play","moduleId":"viewmodels/play","title":"Play","nav":true},
                 /*{durandal:routes}*/
             ]).buildNavigationModel();
             
