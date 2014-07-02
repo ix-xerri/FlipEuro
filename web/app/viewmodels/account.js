96f14ea4669d9e4881365e339efd81e202b68085
ix-xerri <matthewxerri@Matthews-MacBook-Pro.local>
2014-07-02 08:28:49 +0200

    Checking session before loading account

--- web/app/viewmodels/account.js
+++ web/app/viewmodels/account.js
@@ -28,18 +28,27 @@ define(["jquery", "knockout", "durandal/app", "durandal/system", "plugins/router
         },
         
         addCreditCard = function(){
-            var creditCards = this.creditCards();
+            var creditCards = this.creditCards(),
+                creditCard = {
+                    id: Date.now(),
+                    cardNumber: this.cardNumber(),
+                    nameOnCard: this.nameOnCard(),
+                    expirationDate: this.expirationDate()
+                };
+            
             // validation
             if(!validateCreditCard(this.cardNumber())){
-                alert('Please enter a valid credit card number.')
+                app.showMessage('Please enter a valid credit card number.')
                 return;
             };
+            
             if(this.nameOnCard().length === 0) {
-                alert('Please enter the name shown on the credit card.');
+                app.showMessage('Please enter the name shown on the credit card.');
                 return;
             };
+            
             if(parseDate(this.expirationDate()).getTime() < Date.now()){
-                alert('Please enter a valid expiration date');
+                app.showMessage('Please enter a valid expiration date');
                 return;
             }
             
@@ -47,63 +56,71 @@ define(["jquery", "knockout", "durandal/app", "durandal/system", "plugins/router
                 for(var prop in this.creditCards()[item]) {
                     if(this.creditCards()[item].hasOwnProperty(prop)) {
                         if(this.creditCards()[item][prop] === this.cardNumber()) {
-                            alert('found');
+                            app.showMessage('Credit card already exists');
                             return;
                         }
                     }
                 }
             }
             
-            this.creditCards.push({
-                id: Date.now(),
-                cardNumber: this.cardNumber(),
-                nameOnCard: this.nameOnCard(),
-                expirationDate: this.expirationDate()
-            });
+            this.creditCards.push(creditCard);
+            
+            session.account.creditCards.push(creditCard);
+            
+            //todo: needs to be saved to the database but not necessary here since items are saved in memeory
+            
             return;
         },
         
         deposit = function(){
-            appDb.accounts[0].balance.amount = parseFloat(this.currentAmount()) + parseFloat(this.depositAmount());
-            console.log(this.currentAmount());
+            var accountId = session.accountId,
+                selectedCreditCard = appDb.accounts[accountId]['creditCards'][this.depositFromCard()];
+            
+            if(!this.depositFromCard()){
+                app.showMessage('Please add/select a credit card.');
+                return;
+            }
+            
+//            $.post("/api/creditcard/" + depositFromCard() + "/validate/", selectedCreditCard, function(response) {
+                appDb.accounts[accountId].balance.amount = parseFloat(this.currentAmount()) + parseFloat(this.depositAmount());
+//            }
         },
         
         parseDate = function(date) {
             var parts = date.split('-');
             return new Date(parts[0], parts[1]-1, parts[2]);
-        }
-
-        // Lifecycle
-
-        activate = function () {
         },
-
-        deactivate = function () {
-        };
+        
+        activate = function(){
+            if(typeof session.account !== 'object'){
+                router.navigate('login');
+                return;
+            }
+            
+            this.creditCards(session.account['creditCards']);
+            this.currentCurrency(session.account.balance.currency);
+            this.currentAmount(session.account.balance.amount)
+        }
 
     return {
         // Place your public properties here
-        creditCards: ko.observableArray([
-            {id: 0, cardNumber: "4716839212897076", nameOnCard: "Matthew Xerri", expirationDate: "2016-09"},
-            {id: 1, cardNumber: "4506106136972912", nameOnCard: "Matthew Xerri", expirationDate: "2016-09"}
-        ]),
+        creditCards: ko.observableArray(),
         
         // add card
-        cardNumber: ko.observable(""),
-        nameOnCard: ko.observable(""),
-        expirationDate: ko.observable(""),
+        cardNumber: ko.observable(),
+        nameOnCard: ko.observable(),
+        expirationDate: ko.observable(),
         
         addCreditCard: addCreditCard,
         
         // deposit
-        depositFromCard: ko.observable(""),
-        depositAmount: ko.observable(""),
-        currentCurrency: ko.observable(appDb.accounts[0].balance.currency),
-        currentAmount: ko.observable(appDb.accounts[0].balance.amount),
+        depositFromCard: ko.observable(),
+        depositAmount: ko.observable(),
+        currentCurrency: ko.observable(),
+        currentAmount: ko.observable(),
         
         deposit: deposit,
         
-        activate: activate,
-        deactivate: deactivate
+        activate: activate
     };
 });
